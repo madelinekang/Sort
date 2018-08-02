e9ca9b622543684cca9602f5d121ad6f08e99835
Madeline Kang <maddyjkang@gmail.com>
2018-08-02 21:17:53 +0000

    sorts first names alphabetically

---
+++ sort.js
@@ -0,0 +1,18 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+console.log(students.sort(sortNames));
+
+function sortNames(a, b) {
+  //initial input would be object[0] + object[1]
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+}
